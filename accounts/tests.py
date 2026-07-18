86c84a6a39b157c5d30a99add95a47bffc481c9d
chhayanshu19 <lathiyachhayanshu@gmail.com>
2026-07-18 10:46:46 +0530

    feat(auth): implement JWT authentication
    - Added JWT login endpoint
    - Added token refresh endpoint
    - Added authentication tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -17,3 +17,23 @@ class RegisterTest(APITestCase):
 
         self.assertEqual(response.status_code,status.HTTP_201_CREATED)
         self.assertEqual(User.objects.count(),1)
+    
+    def test_login_user(self):
+        User.objects.create_user(
+            username="john",
+            email="john@test.com",
+            password="Password123"
+        )
+
+        response=self.client.post(
+            "/api/auth/login/",
+            {
+                "username":"john",
+                "password":"Password123"
+            },
+            format="json"
+        )
+
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertIn("access",response.data)
+        self.assertIn("refresh",response.data)
